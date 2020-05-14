10edb988d565851d0551e8eb738363a50abd6184
Alexey Vovchenko <blackpantific@gmail.com>
2020-05-14 20:37:06 +0300

    Практика

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+     
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+             using (DbModels dbModel = new DbModels())
+             {
+                var listOfUsers = dbModel.User.ToList();
+                var listOfUsersWithCount = listOfUsers.Select(item =>
+               {
+                   return new UserCount
+                   {
+                       Id = item.Id,
+                       Name = item.Name,
+                       Date = item.Date,
+                       Description = item.Description,
+                       Email = item.Email,
+                       Count = listOfUsers.Count(email => email.Email == item.Email)
+                   };
+               });
+
+                var usersWithCount = new List<UserCount>();
+
+                foreach (var item in listOfUsersWithCount)
+                {
+                    usersWithCount.Add(item);
+                }
+
+
+                 return View(usersWithCount);
+             }
+           
+        }
+
+        
+
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                userModel.Date = DateTime.Now;
+                dbModel.User.Add(userModel);
+                dbModel.SaveChanges();
+            }
+
+            ModelState.Clear();
+            /*return View("AddOrEdit", new User());*/
+            return RedirectPermanent("~/Home/Contact");
+        }
+    }
+}
